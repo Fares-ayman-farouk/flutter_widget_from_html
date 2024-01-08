44cfbcb32810b1623004cfd31bf89b601c54c1a6
Fares-ayman-farouk <f.ayman@suiiz.com>
2024-01-08 16:35:44 +0200

    سي

--- packages/fwfh_cached_network_image/lib/src/cached_network_image_factory.dart
+++ packages/fwfh_cached_network_image/lib/src/cached_network_image_factory.dart
@@ -6,7 +6,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 
-/// A mixin that can render IMG with `cached_network_image` plugin.
 mixin CachedNetworkImageFactory on WidgetFactory {
   /// Uses a custom cache manager.
   BaseCacheManager? get cacheManager => null;
